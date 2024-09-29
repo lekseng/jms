a51d1d2ca2c35dacacced3fc2028ad0c9c87734a
lekseng <isaisa8784@gmail.com>
2024-09-29 21:17:35 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -531,33 +531,6 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 print_success "Password SSH"
 }
 
-function udp_mini(){
-clear
-print_install "Memasang Service Limit IP & Quota"
-wget -q https://raw.githubusercontent.com/lekseng/jms/master/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
-
-# // Installing UDP Mini
-mkdir -p /usr/local/kyt/
-wget -q -O /usr/local/kyt/udp-mini "${REPO}files/udp-mini"
-chmod +x /usr/local/kyt/udp-mini
-wget -q -O /etc/systemd/system/udp-mini-1.service "${REPO}files/udp-mini-1.service"
-wget -q -O /etc/systemd/system/udp-mini-2.service "${REPO}files/udp-mini-2.service"
-wget -q -O /etc/systemd/system/udp-mini-3.service "${REPO}files/udp-mini-3.service"
-systemctl disable udp-mini-1
-systemctl stop udp-mini-1
-systemctl enable udp-mini-1
-systemctl start udp-mini-1
-systemctl disable udp-mini-2
-systemctl stop udp-mini-2
-systemctl enable udp-mini-2
-systemctl start udp-mini-2
-systemctl disable udp-mini-3
-systemctl stop udp-mini-3
-systemctl enable udp-mini-3
-systemctl start udp-mini-3
-print_success "Limit IP Service"
-}
-
 function ssh_slow(){
 clear
 # // Installing UDP Mini
